68d98821cabc0fe0849bf06c4d41ccde117785e3
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-04 15:46:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+
+	// Write your code here  aabbcd
+	let regx=/^[a-z]+$/;
 	let obj={};
-	for(let i=0;i<str.length();i++){
-	if(obj[str[i]!==undefine){
-		obj[str[i]]+=1
-	}
-		else{
-			obj[str[i]]=1;
-		}
-	}
-	for(let i=0;i<str.length();i++){
-		if(obj[str[i]==1]){
-			return str[i];
-		}
+	if(str.length<1000 && str.match(regx)){
+		 for(let i=0;i<str.length;i++){
+	   if(obj[str[i]]!==undefined){
+		   obj[str[i]]+=1;
+	   }
+		   else{
+			   obj[str[i]]=1;
+		   }
+	   }
+	   for(let i=0;i<str.length;i++){
+		   if(obj[str[i]]==1){
+			   return str[i];
+		   }
+	   }
+	   
 	}
 	return "null";
-}
+   }
 const input = prompt("Enter a string");
 window.alert(firstNonRepeatedChar(input));
 
